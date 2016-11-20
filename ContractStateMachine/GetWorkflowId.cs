66dbaaeb4ecce029c3770d348b39791cbe9de12e
abennehhou <asmaabennehhou@gmail.com>
2016-11-20 20:44:22 +0100

    Add WF service project using state machine activities

---
+++ ContractStateMachine/GetWorkflowId.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Activities;
+using log4net;
+
+namespace ContractStateMachine
+{
+    public sealed class GetWorkflowId : CodeActivity
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(GetWorkflowId));
+
+        public OutArgument<Guid> WorkflowId { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var workflowId = context.WorkflowInstanceId;
+            log.Debug($"Workflow Instance Id: {workflowId}.");
+            context.SetValue(WorkflowId, workflowId);
+        }
+    }
+}
---
+++ ContractStateMachine/LogMessageDebug.cs
@@ -0,0 +1,23 @@
+﻿using System.Activities;
+using log4net;
+
+namespace ContractStateMachine
+{
+    public sealed class LogMessageDebug : CodeActivity
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LogMessageDebug));
+
+        public InArgument<string> Message { get; set; }
+
+        // Si votre activité retourne une valeur, dérivez de CodeActivity<TResult>
+        // et retournez la valeur à partir de la méthode Execute.
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtenir la valeur runtime de l'argument d'entrée Text
+            string text = context.GetValue(Message);
+
+            var workflowId = context.WorkflowInstanceId;
+            log.Debug($"[{workflowId}] {text}");
+        }
+    }
+}
